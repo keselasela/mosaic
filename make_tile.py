814cb6270bed797763f24ef9d9d753bcc0ac0fca
keselasela <1230zakiyama@gmail.com>
2019-05-03 21:16:41 +0900

    make_tile_list

---
+++ make_tile.py
@@ -0,0 +1,20 @@
+
+from PIL import Image
+import math
+def make_tile_list(target_image, tile_num=(100,100) ):
+    x_tile_num = tile_num[0]
+    y_tile_num = tile_num[1]
+    x_target_size = target_image.size[0]
+    y_target_size = target_image.size[1]
+    tile_list = []
+    for y in range(0, y_tile_num):
+        for x in range(0, x_tile_num):
+            tile = (x*x_target_size/x_tile_num, y*y_target_size/y_tile_num,
+            (x+1)*x_target_size/x_tile_num, (y+1)*y_target_size/y_tile_num )
+            tile = tuple(math.floor(i) for i in tile)
+            tile_list.append(tile)
+    return tile_list
+
+if __name__ == "__main__":
+    img = Image.open( "img_0.jpg" )
+    print(make_tile_list(img))
